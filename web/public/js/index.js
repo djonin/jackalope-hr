cd1a5c67babc23953d3604aba3c25fa2a3fdb8be
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-08-14 13:56:49 -0700

    library

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,26 +1,83 @@
 var projStorage = {};
+var elemStorage = {};
 
-$(document).ready(function() {
-	var socket = io.connect(window.location.origin);
-	socket.on('update', function(notification) {
-		console.log(notification);
-		if((notification.type === 'project')&&(notification.action === 'new')) {
-			var elem = $('<li>'+notification.data.name+'</li>');
-			elem.on('click', selectProject);
-			$('#projects').append(elem);
-			projStorage[notification.data.id] = { name: notification.data.name };
-		} else if(notification.type === 'notification') {
-			var id = notification.target;
-			if(projStorage[id]) {
-				if(!projStorage[id].notifications) {
-					projStorage[id].notifications = [];
-				}
-				projStorage[id].notifications.push(notification.data);
+
+var CSE = function(addr, newProjCB, compProjCB, newNotCB) {
+	this.socket = io.connect(addr);
+	this.onNewProject = newProjCB || function(){};
+	this.onNewNotification = newNotCB || function(){};
+	this.onProjectComplete = compProjCB || function(){};
+}
+
+
+CSE.prototype.constructor = CSE;
+
+CSE.prototype.subscribe = function(clientId) {
+	var self = this;
+	this.socket.on('update', function(notification) {
+		if(notification.type === 'project') {
+			if(notification.action === 'complete') {
+				self.onProjectComplete(notification.target);
+			} else if(notification.action === 'new') {
+				self.onNewProject(notification.data.id, notification.data.name);
 			}
+		} else if(notification.type === 'notification') {
+			self.onNewNotification(notification.target, notification.data);
 		}
 	});
-	socket.emit('subscribe', 'randomid');
+	this.socket.emit('subscribe', clientId);
+}
 
+CSE.prototype.setOnNewNotification = function(callback) {
+	if(!callback) {
+		callback = function(){};
+	}
+	this.onNewNotification = callback;
+}
+
+CSE.prototype.setOnProjectComplete = function(callback) {
+	if(!callback) {
+		callback = function(){};
+	}
+	this.onProjectComplete = callback;
+}
+
+CSE.prototype.setOnNewProject = function(callback) {
+	if(!callback) {
+		callback = function(){};
+	}
+	this.onNewProject = callback;
+}
+
+
+
+
+$(document).ready(function() {
+	var cse = new CSE(window.location.origin);
+	cse.subscribe('randomid');
+
+	cse.setOnNewNotification(function(target,data) {
+		if(projStorage[target]) {
+			if(!projStorage[target].notifications) {
+				projStorage[target].notifications = [];
+			}
+			projStorage[target].notifications.push(data);
+		}
+	});
+
+	cse.setOnNewProject(function(id, name) {
+		var elem = $('<li>'+name+'</li>');
+		elem.on('click', selectProject);
+		$('#projects').append(elem);
+		projStorage[id] = { name: name };
+		elemStorage[id] = { elem: elem };
+	});
+
+	cse.setOnProjectComplete(function(id) {
+		elemStorage[id].elem.remove;
+		delete elemStorage[id];
+		delete projStorage[id];
+	});
 });
 
 var selectProject = function(evt) {
